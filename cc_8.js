84e8941611ad0ebf60bedd9386dd8d4365e870e5
Karim <konanfoi@gmail.com>
2025-02-16 17:57:22 -0500

    Task 2 - Product Price After Discount.

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,11 @@ function calculateSalary(baseSalary, bonus, taxRate) { //write a function
 }
 console.log(calculateSalary(5000, 500, 0.1)); //logs
 console.log(calculateSalary(7000, 1000, 0.15));
+
+//Task 2
+let calculateDiscount = function(price, discountRate) { //declare a function exression
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`; //to two decimals
+};
+console.log(calculateDiscount(100, 0.2)); //logs
+console.log(calculateDiscount(250, 0.15));
